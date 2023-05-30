2069fa102bb9f5ce0df70c76844ab0195032b665
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-05-30 21:51:11 +0100

    Registered database context

--- RestaurantMVCNET/Program.cs
+++ RestaurantMVCNET/Program.cs
@@ -5,11 +5,14 @@ namespace RestaurantMVCNET
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<MvcRestaurantContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcRestaurantContext")));
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
 
             app.Run();
+
         }
     }
 }
