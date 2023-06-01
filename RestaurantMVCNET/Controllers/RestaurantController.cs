0979fad9d2841040db3fb9902683d2f8c47d2334
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-06-01 13:37:42 +0100

    Added HttpGet attribute

---
+++ RestaurantMVCNET/Controllers/RestaurantController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantMVCNET.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
