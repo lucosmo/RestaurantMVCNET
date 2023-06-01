822dcd708a4323f608a3d4bfb123e238a2665df5
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-06-01 13:37:16 +0100

    added tableID

--- RestaurantMVCNET/Models/Reservation.cs
+++ RestaurantMVCNET/Models/Reservation.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public int RestaurantId { get; set; }
-        public Customer MainCustomer { get; set; }
+        public int CustomerId { get; set; }
+        public int TableId { get; set; }
+        public Customer Customer { get; set; }
         public string NumberOfPeople { get; set; }
         public DateTime TimeOfReservation { get; set; }
         public string Comments { get; set; }
