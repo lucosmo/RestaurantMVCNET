80b62e71a1411c88333c159bbce2e1a951bbdbff
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-06-09 13:52:38 +0100

    added db initializer with 1 row (restaurant obj)

---
+++ RestaurantMVCNET/Data/DbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using RestaurantMVCNET.Models;
+
+namespace RestaurantMVCNET.Data
+{
+    public class DbInitializer
+    {
+        public static async Task Initialize(MvcRestaurantContext context)
+        {
+
+            if (context.Restaurants.Any())
+            {
+                return;
+            }
+
+            var restaurants = new Restaurant[]
+            {
+                new Restaurant { Name = "Forni Rossi", Description="desc1", Address="adres1", Capacity=2, Email="edwdwd@ewew.lp", PhoneNumber="342432442", Tables=null},
+                
+            };
+
+            await context.Restaurants.AddRangeAsync(restaurants);
+            await context.SaveChangesAsync();
+        }
+    }
+}
