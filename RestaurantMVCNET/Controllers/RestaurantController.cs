e8e90a0e1107d4ece57c8c4d8e0deddec5dd4bcd
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-06-09 07:48:43 +0100

    created 'create' controller

--- RestaurantMVCNET/Controllers/RestaurantController.cs
+++ RestaurantMVCNET/Controllers/RestaurantController.cs
@@ -1,14 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantMVCNET.Models;
+using RestaurantMVCNET.Services;
 
 namespace RestaurantMVCNET.Controllers
 {
     public class RestaurantController : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
-        {
+        private readonly IRestaurantService _db;
 
+        public RestaurantController(IRestaurantService db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var allRestaurants = await _db.GetAllRestaurants();
+            return View(allRestaurants);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Restaurant restuarant)
+        {
+            try
+            {
+                await _db.AddRestaurant(restuarant);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+
+
+                ErrorViewModel addingError = new ErrorViewModel();
+                addingError.Message = ex.Message;
+                return View("Error", addingError);
+
+
+            }
         }
     }
 }
