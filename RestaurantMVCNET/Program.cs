0809c946015f52a5d41e350d1d2f21439b24ef6a
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-06-09 07:19:12 +0100

    Added Restaurant service, app settings

--- RestaurantMVCNET/Program.cs
+++ RestaurantMVCNET/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RestaurantMVCNET.Data;
+using RestaurantMVCNET.Services;
 
 namespace RestaurantMVCNET
 {
@@ -8,11 +9,25 @@ namespace RestaurantMVCNET
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
             builder.Services.AddDbContext<MvcRestaurantContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcRestaurantContext")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcRestaurantContext")), ServiceLifetime.Transient);
+
+            builder.Services.AddTransient<IRestaurantService, RestaurantService>();
+
             var app = builder.Build();
 
             //app.MapGet("/", () => "Hello World!");
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Restaurant}/{action=Index}/{id?}");
