6db6a22b4aa5f1ecbab9cc009de854ba4990969b
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-06-09 13:53:36 +0100

    attribute for ID set to [DatabaseGenerated(DatabaseGeneratedOption.Identity)] Autoincrement of id still not working

--- RestaurantMVCNET/Models/Restaurant.cs
+++ RestaurantMVCNET/Models/Restaurant.cs
@@ -1,7 +1,13 @@
-﻿namespace RestaurantMVCNET.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RestaurantMVCNET.Models
 {
     public class Restaurant
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
@@ -9,7 +15,7 @@
         public int Capacity { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set;}
-
+        [AllowNull]
         public IEnumerable<Table> Tables { get; set; }
     }
 }
